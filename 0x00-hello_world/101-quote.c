7a5f9163d0167f5f55cdf24a53fc187e9d2c8040
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-06-20 16:32:17 -0700

    final commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: Print a quote using the write function
- * write(int fd, const void *buf, size_t count)
- *
- * Return: 1 (failure)
- */
+* main - Entry point
+*
+* Description: print a quote using the write function
+* ssize_t write(int fd, const void *buf, size_t count);
+*
+* Return: 1 (error).
+*/
+
 int main(void)
 {
-    char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(1, quo, sizeof(quo) - 1);
-    return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
