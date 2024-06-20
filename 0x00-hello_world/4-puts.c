fcab64e5f56258780871520c5a232cb4e706c9d7
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-06-20 12:14:39 -0700

    3bgytre

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints "Programming is like building
+ * a multilingual puzzle", followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
