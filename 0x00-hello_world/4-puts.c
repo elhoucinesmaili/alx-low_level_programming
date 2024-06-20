73f8091fb64f77dd447911149e2517d5feb60c0f
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-06-20 12:33:35 -0700

    tydggdr

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: Program that prints "Programming is like building
- * a multilingual puzzle", followed by a new line.
+ * Description: A C program that prints with the puts function
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
