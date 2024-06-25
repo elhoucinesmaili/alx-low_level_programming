6f950e07bd889aae382d24de567895427c5b5c61
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-06-25 08:27:02 -0700

    hdhs

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,33 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Included Libraries */
-#include <unistd.h>
+/* Included libraries */
+#include <stdio.h>
 
-/* Function Prototypes */
-
-/* _putchar - writes the character c to stdout */
+/* Function prototypes */
 int _putchar(char c);
-
-/* 0. _putchar.c */
-void print_alphabet(void);
-void print_alphabet_x10(void);
-
-/* 1. 1-alphabet.c */
-int _islower(int c);
-
-/* 2. 2-print_alphabet_x10.c */
-int _isalpha(int c);
-
-/* 3. 3-islower.c */
-int print_sign(int n);
-
-/* 4. 5-sign.c */
-int _abs(int);
-
-/* 5. 6-abs.c */
 int print_last_digit(int);
-
-/* 6. 7-print_last_digit.c */
 
 #endif /* MAIN_H */
