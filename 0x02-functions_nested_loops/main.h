b702f210d26d06ba45a792b4348fedb8f3ccbd1e
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-06-25 08:02:56 -0700

    rfsg

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Included libraries */
+#include <unistd.h>
+
+/* Function prototypes */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int n);
 
 #endif /* MAIN_H */
