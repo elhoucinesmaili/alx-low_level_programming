df89fc6a9222cf3fdc9c88bc2eca6511b3882e7c
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-06-25 08:10:20 -0700

    rtez

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,30 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Included libraries */
-#include <unistd.h>
+#include <unistd.h> /* For the system call write */
+#include <stdio.h>  /* For printf function */
 
-/* Function prototypes */
+/* Function Prototypes */
+
+/* _putchar: Writes a character to stdout */
 int _putchar(char c);
+
+/* print_alphabet: Prints the alphabet in lowercase */
 void print_alphabet(void);
+
+/* print_alphabet_x10: Prints the alphabet in lowercase 10 times */
 void print_alphabet_x10(void);
+
+/* _islower: Checks if a character is lowercase */
 int _islower(int c);
+
+/* _isalpha: Checks if a character is alphabetic */
 int _isalpha(int c);
+
+/* print_sign: Prints the sign of a number (+, 0, or -) */
 int print_sign(int n);
+
+/* _abs: Computes the absolute value of an integer */
+int _abs(int);
 
 #endif /* MAIN_H */
