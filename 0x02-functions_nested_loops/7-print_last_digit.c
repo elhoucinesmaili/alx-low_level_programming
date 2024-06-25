9e60d523755592d9277480930be39872dd3482e8
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-06-25 08:43:07 -0700

    rdds

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,18 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: the number to find the last digit of
+ * @n: integer whose last digit is to be printed
  *
- * Return: the value of the last digit
+ * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
-	int last_digit;
+	int last_digit = n % 10;
 
-	n = _abs(n); /* Assuming _abs function is properly defined in main.h */
+	if (last_digit < 0)
+		last_digit = -last_digit;
 
-	last_digit = n % 10;
-	_putchar(last_digit + '0');
+	_putchar('0' + last_digit);
 
-	return (last_digit); /* Return statement with parentheses */
+	return (last_digit);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Included libraries */
-#include <stdio.h>
+/* Including standard library for size_t definition */
+#include <stddef.h>
 
-/* Function prototypes */
+/* _putchar - writes the character c to stdout */
 int _putchar(char c);
-int print_last_digit(int);
 
-#endif /* MAIN_H */
+/* 0. _putchar */
+void _putchar(void);
+
+/* 1. I sometimes suffer from insomnia */
+void print_alphabet(void);
+
+/* 2. 10 x alphabet */
+void print_alphabet...
