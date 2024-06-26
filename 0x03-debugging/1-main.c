50fb1fcb8395d6b97df50cdbb20f507377a80dfe
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-06-26 12:17:48 -0700

    fdhd

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	/* Commenting out the while loop to prevent infinite loop */
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
