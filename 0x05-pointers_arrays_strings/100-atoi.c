ace4b0c98436c17bdea82de2ae0b2989ce5cc599
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-07-01 07:35:10 -0700

    hdgd

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1; /* Sign of the number, initialized to positive */
+	int num = 0;  /* Resulting number */
+
+	/* Skip whitespace characters */
+	while (*s == ' ' || (*s >= 9 && *s <= 13))
+		s++;
+
+	/* Handle optional sign */
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+
+	/* Process digits and convert to integer */
+	while (*s >= '0' && *s <= '9')
+	{
+		num = num * 10 + (*s - '0');
+		s++;
+	}
+
+	return (num * sign);
+}
