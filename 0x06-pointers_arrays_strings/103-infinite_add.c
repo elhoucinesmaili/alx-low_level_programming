9eac943ee9dfacaba1aeff66dbd4713acb453943
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-07-03 13:33:14 -0700

    hdbd

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string in ROT13
- * @s: String to be encoded
- * Return: Pointer to the resulting string
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ * Return: the resulting string
  */
 char *rot13(char *s)
 {
-	int i, j;
+	/* Define ROT13 encoding arrays */
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
 
+	/* Iterate through each character in the string */
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		/* Check if s[i] is an alphabetic character */
-		if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
+		/* Check if the character is in array 'a' */
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			for (j = 0; a[j] != '\0'; j++)
+			if (s[i] == a[j])
 			{
-				if (s[i] == a[j])
-				{
-					s[i] = b[j];
-					break;
-				}
+				s[i] = b[j]; /* Perform ROT13 substitution */
+				break;
 			}
 		}
 	}
-	return (s);
+
+	return (s); /* Return the modified string */
 }
