c918f405e1239d838b4d554415e98a79d4a0423f
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-07-08 07:00:03 -0700

    hdbed

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,9 +3,11 @@
 /**
  * _strchr - locates a character in a string
  * @s: the string to search
- * @c: the character to locate
+ * @c: the character to find
  *
- * Return: pointer to the first occurrence of c in s, or NULL if not found
+ * Return: a pointer to the first occurrence of
+ * the character c in the string s,
+ * or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
@@ -17,9 +19,11 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	if (*s == c)
+
+	if (c == '\0')
 	{
 		return (s);
 	}
+
 	return (NULL);
 }
