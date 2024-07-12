0519032a53c0eb6f9589c76f6c75293b1bb21ed1
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-07-12 08:13:39 -0700

    hbdgd

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Argument count
+ * @argv: Argument vector (array of arguments)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
