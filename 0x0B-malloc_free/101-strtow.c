3a78f8c310aa511192e3b350ae2892bbf84db44d
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-07-15 10:44:49 -0700

    hdgdge

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,101 +1,93 @@
 #include "main.h"
 
 /**
-*strtow - splits a stirng into words
-*@str: string to be splitted
-*
-*Return: pointer to the array of splitted words
-*/
-
+ * strtow - Splits a string into words
+ * @str: The string to be split
+ *
+ * Return: Pointer to the array of split words, or NULL on failure
+ */
 char **strtow(char *str)
 {
-char **split;
-int i, j = 0, temp = 0, size = 0, words = num_words(str);
+	char **split;
+	int i, j = 0, size = 0, words = num_words(str);
 
-if (words == 0)
-return (NULL);
-split = (char **) malloc(sizeof(char *) * (words + 1));
-if (split != NULL)
-{
-for (i = 0; i <= len(str) && words; i++)
-{
-if ((str[i] != ' ') && (str[i] != '\0'))
-size++;
-else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-{
-split[j] = (char *) malloc(sizeof(char) * size + 1);
-if (split[j] != NULL)
-{
-while (temp < size)
-{
-split[j][temp] = str[(i - size) +temp];
-temp++;
-}
-split[j][temp] = '\0';
-size = temp = 0;
-j++;
-}
-else
-{
-while (j-- >= 0)
-free(split[j]);
-free(split);
-return (NULL);
-}
-}
-}
-split[words] = NULL;
-return (split);
-}
-else
-return (NULL);
-}
+	if (words == 0)
+		return (NULL);
 
+	split = (char **)malloc(sizeof(char *) * (words + 1));
+	if (split == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len(str); i++)
+	{
+		if (str[i] != ' ' && str[i] != '\0')
+			size++;
+		else if ((str[i] == ' ' || str[i] == '\0') && i && str[i - 1] != ' ')
+		{
+			split[j] = (char *)malloc(sizeof(char) * (size + 1));
+			if (split[j] != NULL)
+			{
+				for (int temp = 0; temp < size; temp++)
+					split[j][temp] = str[i - size + temp];
+
+				split[j][size] = '\0';  /* Null-terminate the word */
+				size = 0;               /* Reset size for next word */
+				j++;
+			}
+			else
+			{
+				while (j-- > 0)
+					free(split[j]);
+				free(split);
+				return (NULL);
+			}
+		}
+	}
+	split[words] = NULL; /* Set the last element to NULL */
+	return (split);
+}
 
 /**
-* num_words - counts the number of words in str
-*@str: string to be used
-*
-*Return: number of words
-*/
+ * num_words - Counts the number of words in a string
+ * @str: The string to be analyzed
+ *
+ * Return: The number of words
+ */
 int num_words(char *str)
 {
-int i = 0, words = 0;
+	int i = 0, words = 0;
 
-while (i <= len(str))
-{
-if ((str[i] != ' ') && (str[i] != '\0'))
-{
-i++;
-}
-else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-{
-words += 1;
-i++;
-}
-else
-{
-i++;
-}
-}
-return (words);
+	while (i <= len(str))
+	{
+		if (str[i] != ' ' && str[i] != '\0')
+		{
+			i++;
+		}
+		else if ((str[i] == ' ' || str[i] == '\0') && i && str[i - 1] != ' ')
+		{
+			words++;
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (words);
 }
 
 /**
-* len - returns length of str
-*@str: string to be counted
-*
-* Return: length of the string
-*/
-
+ * len - Returns the length of a string
+ * @str: The string to be measured
+ *
+ * Return: Length of the string
+ */
 int len(char *str)
 {
-int len = 0;
+	int length = 0;
 
-if (str != NULL)
-{
-while (str[len])
-len++;
-}
-return (len);
+	while (str && str[length])
+		length++;
+
+	return (length);
 }
