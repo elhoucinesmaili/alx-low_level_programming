a35020369cdc2572e2f59d286489912555a18a36
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-07-18 11:27:00 -0700

    hdgdgd

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
 
 /**
  * is_digit - checks if a string contains a non-digit char
@@ -8,13 +12,13 @@
  */
 int is_digit(char *s)
 {
-	int i = 0;
+	int index = 0;
 
-	while (s[i])
+	while (s[index])
 	{
-		if (s[i] < '0' || s[i] > '9')
+		if (s[index] < '0' || s[index] > '9')
 			return (0);
-		i++;
+		index++;
 	}
 	return (1);
 }
@@ -27,11 +31,13 @@ int is_digit(char *s)
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int index = 0;
 
-	while (s[i] != '\0')
-		i++;
-	return (i);
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	return (index);
 }
 
 /**
@@ -53,7 +59,7 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, index, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -64,8 +70,8 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
+	for (index = 0; index <= len1 + len2; index++)
+		result[index] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
@@ -80,16 +86,16 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)
+	for (index = 0; index < len - 1; index++)
 	{
-		if (result[i])
+		if (result[index])
 			a = 1;
 		if (a)
-			putchar(result[i] + '0');
+			_putchar(result[index] + '0');
 	}
 	if (!a)
-		putchar('0');
-	putchar('\n');
+		_putchar('0');
+	_putchar('\n');
 	free(result);
 	return (0);
 }
