0159d9cd1eb1da4510243535d3635175ca37aede
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-07-22 12:02:23 -0700

    hdgdg

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,7 @@
  * Description: This function initializes the members of a struct dog
  * with the given values.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner);
 {
 	if (d != NULL)
 	{
