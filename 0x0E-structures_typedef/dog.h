b293e1bc4d4bd7af1df97a5e2e8a696647f667bb
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-07-22 12:10:40 -0700

    hdgd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,33 @@
 #define DOG_H
 
 /**
- * struct dog - A structure to store information about a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct car - car class
+ *
+ * @make: Make of the car
+ *
+ * @model: Model of the car
+ *
+ * @year: Year of manufacture
+ *
+ * @owner: Owner of the car
  */
-struct dog
+
+struct car
 {
-	char *name;
-	float age;
+	char *make;
+	char *model;
+	int year;
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-#endif /* DOG_H */
+void init_car(struct car *c, char *make, char *model, int year, char *owner);
+void print_car(struct car *c);
+
+typedef struct car car_t;
+
+car_t *new_car(char *make, char *model, int year, char *owner);
+
+void free_car(car_t *c);
+
+#endif
+
