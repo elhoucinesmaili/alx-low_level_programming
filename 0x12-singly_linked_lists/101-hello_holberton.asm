1c133818a2be18a110f52b986444d59ccaa0d0c9
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-08-01 11:11:45 -0700

    hegdgd

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,15 @@
 SECTION .data
-message db "Hello, Holberton", 0
-format db "%s", 10, 0
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-SECTION .text
-extern printf
-global _start
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-_start:
-    ; Load the address of the message into the RSI register
-    mov rsi, message
-    ; Load the address of the format string into the RDI register
-    mov rdi, format
-    ; Clear the RAX register (no floating point arguments)
-    xor rax, rax
-    ; Call the printf function
-    call printf
-    ; Exit the program (if using Linux system calls)
-    ; Move exit code 0 into RDI register
-    mov rdi, 0
-    ; Use syscall number for exit (60) into RAX register
-    mov rax, 60
-    ; Perform the syscall
-    syscall
+	mov eax, 0
+	ret
