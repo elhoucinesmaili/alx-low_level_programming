ac57aca1c33e3ddfe8be9be80ae80cd04c827a4e
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-08-06 08:29:59 -0700

    dhddvd

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,73 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listp - frees a linked list of listp_t
- * @head: pointer to the head of the list.
+ * print_listint_safe - prints a listint_t linked list safely (handles loops)
+ * @head: pointer to the head of the list
  *
- * Return: no return.
- */
-void free_listp(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *curr;
-
-	if (head != NULL)
-	{
-		curr = *head;
-		while ((temp = curr) != NULL)
-		{
-			curr = curr->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * print_listint_safe - prints a linked list safely
- * @head: pointer to the head of the list.
- *
- * Description: The function prints a list while avoiding infinite loops by
- * keeping track of previously seen nodes.
- * Return: the number of nodes in the list.
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *slow = head;
+	const listint_t *fast = head;
 	size_t count = 0;
-	size_t index = 0;
-	listp_t *hptr = NULL;
-	listp_t *new;
-	listp_t *add;
-	unsigned int i;
-	unsigned int flag = 0;
 
-	while (head != NULL)
+	while (slow != NULL)
 	{
-		new = malloc(sizeof(listp_t));
-		if (new == NULL)
-			exit(98);
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
 
-		new->p = (void *)head;
-		new->next = hptr;
-		hptr = new;
-
-		add = hptr;
-		while (add->next != NULL)
+		slow = slow->next;
+		if (fast && fast->next)
 		{
-			add = add->next;
-			if (head == add->p)
+			fast = fast->next->next;
+			if (slow == fast)
 			{
-				printf("-> [%p] %d\n", (void *)head, head->n);
-				free_listp(&hptr);
-				return (count);
+				printf("-> [%p] %d\n", (void *)slow, slow->n);
+				break;
 			}
 		}
-
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		count++;
+		else
+		{
+			fast = NULL;
+		}
 	}
 
-	free_listp(&hptr);
+	if (slow && fast && slow != fast)
+	{
+		while (slow != fast)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			slow = slow->next;
+			count++;
+		}
+		printf("-> [%p] %d\n", (void *)slow, slow->n);
+	}
+
 	return (count);
 }
