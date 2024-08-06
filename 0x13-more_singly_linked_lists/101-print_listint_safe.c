994f60c0e8b151b6234abe8dc535ef9aaa5c3d1c
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2024-08-06 08:39:47 -0700

    jddg

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,50 +1,91 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * print_listint_safe - prints a listint_t linked list safely (handles loops)
- * @head: pointer to the head of the list
+ * print_listint_safe - Prints a listint_t linked list, even if it has a loop.
+ * @head: Pointer to the head of the list.
  *
- * Return: number of nodes in the list
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head;
-	const listint_t *fast = head;
+	const listint_t *current = head;
+	const listint_t *checker;
 	size_t count = 0;
 
-	while (slow != NULL)
+	while (current)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
+		checker = head;
 
-		slow = slow->next;
-		if (fast && fast->next)
+		while (checker != current)
 		{
-			fast = fast->next->next;
-			if (slow == fast)
+			if (checker == current->next)
 			{
-				printf("-> [%p] %d\n", (void *)slow, slow->n);
-				break;
+				printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+				return (count);
 			}
+			checker = checker->next;
 		}
-		else
-		{
-			fast = NULL;
-		}
+		current = current->next;
 	}
+	return (count);
+}
 
-	if (slow && fast && slow != fast)
+/**
+ * add_nodeint - Adds a new node at the beginning of a listint_t list.
+ * @head: Double pointer to the head of the list.
+ * @n: Value to add to the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+
+/**
+ * free_listint_safe - Frees a listint_t list safely (handles loops).
+ * @h: Double pointer to the head of the list.
+ *
+ * Return: The size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current, *checker;
+	size_t count = 0;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
 	{
-		while (slow != fast)
-		{
-			printf("[%p] %d\n", (void *)slow, slow->n);
-			slow = slow->next;
-			count++;
-		}
-		printf("-> [%p] %d\n", (void *)slow, slow->n);
-	}
+		current = *h;
+		checker = *h;
 
+		while (checker != current)
+		{
+			if (checker == current->next)
+			{
+				*h = NULL;
+				return (count);
+			}
+			checker = checker->next;
+		}
+		*h = (*h)->next;
+		free(current);
+		count++;
+	}
+	*h = NULL;
 	return (count);
 }
