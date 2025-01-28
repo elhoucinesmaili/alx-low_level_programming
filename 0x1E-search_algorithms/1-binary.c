3797452fae3e8a9475a31a2679dbcaae7e12a940
elhoucinesmaili <elhoucinesmaili51@gmail.com>
2025-01-28 07:39:49 -0800

    git

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,38 +1,50 @@
 #include "search_algos.h"
+#include <stdio.h>
 
 /**
- * binary_search - searches for a value in a sorted array of integers,
- * using the Binary search algorithm
- * @array: is a pointer to the first element of the array to search in
- * @size: is the number of elements in array
- * @value: is the value to search for
- * Return: the index where value is located
-**/
-
+ * binary_search - Searches for a value in a sorted array of integers
+ *                 using the Binary search algorithm.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in the array.
+ * @value: Value to search for.
+ *
+ * Return: The index where the value is located,
+ *         or -1 if the value is not present or the array is NULL.
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, left, right;
+    size_t mid, left, right;
 
-	if (!array)
-		return (-1);
+    if (!array)
+        return (-1);
 
-	for (left = 0, right = size - 1; right >= left;)
-	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
+    left = 0;
+    right = size - 1;
 
-		printf("%d\n", array[i]);
+    while (left <= right)
+    {
+        /* Print the current search range */
+        printf("Searching in array: ");
+        for (size_t i = left; i <= right; i++)
+        {
+            printf("%d", array[i]);
+            if (i < right)
+                printf(", ");
+        }
+        printf("\n");
 
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
+        /* Calculate the middle index */
+        mid = left + (right - left) / 2;
 
-		if (array[i] > value)
-			right = i - 1;
-		else
-			left = i + 1;
-	}
+        if (array[mid] == value)
+            return (mid); /* Return index if value is found */
 
-	return (-1);
+        if (array[mid] < value)
+            left = mid + 1; /* Narrow search to the right subarray */
+        else
+            right = mid - 1; /* Narrow search to the left subarray */
+    }
+
+    return (-1); /* Return -1 if value is not found */
 }
+
